792cd7bcb1720159bc3cc20f307e64b19baf5bb5
annmaevskaya <anna.maevskaya@rebelmouse.com>
2021-11-01 14:54:29 +0100

    OOP p. 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        Car carBublik = new Car("Седан", "бензин", "красный", "QWE789");
+        carBublik.getInfo();
+        Car vladCar = new Car("кроссовер", "'электрокар", "желтый", "asd123");
+        vladCar.getInfo();
     }
+
 }
